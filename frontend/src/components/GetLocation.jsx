0e78208b649643b64bea12b4a2c0550e03151f9e
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2026-01-16 22:12:37 +0200

    getLocation function implemented

---
+++ frontend/src/components/GetLocation.jsx
@@ -0,0 +1,47 @@
+//IMPORTING REACT
+import { useState, useEffect, useContext } from "react";
+//IMPORTING CONTEXTS
+import { LocationContext } from "../contexts/Contexts";
+
+export const getLocation = (city) => {
+  const { curLocation, setCurLocation } = useContext(LocationContext);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    if (curLocation.city || city) {
+      const fetchData = async () => {
+        setIsPending(true);
+        try {
+          const res = await fetch(
+            city
+              ? `https://api.weatherapi.com/v1/current.json?key=${
+                  import.meta.env.VITE_API_KEY
+                }&q=${city}&aqi=no`
+              : `https://api.weatherapi.com/v1/current.json?key=${
+                  import.meta.env.VITE_API_KEY
+                }&q=${curLocation.city}&aqi=no`
+          );
+          if (!res.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const json = await res.json();
+          setData(json);
+          if (city && json?.location) {
+            setCurLocation({
+              city: json.location.region,
+              country: json.location.country,
+            });
+          }
+        } catch (error) {
+          setError(error);
+        } finally {
+          setIsPending(false);
+        }
+      };
+      fetchData();
+    }
+  }, [city]);
+  return [data];
+};
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 import { IoIosSunny, IoMdMoon } from "react-icons/io";
 //IMPORTING REACT
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
+import { getLocation } from "./GetLocation";
 //IMPORTING RRD
 import { Link } from "react-router-dom";
 //IMPORTING CONTEXTS
@@ -14,7 +15,9 @@ const Navbar = () => {
   const { curLocation, setCurLocation } = useContext(LocationContext);
   const [darkMode, setDarkMode] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const [searchTerm, setSearchTerm] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [query, setQuery] = useState("");
+  const [data] = getLocation(query);
 
   //BUTTONS
   const button = ({ body }) => {
@@ -26,12 +29,25 @@ const Navbar = () => {
   };
 
   //FUNCTIONS
+
   function switcher() {
     setDarkMode(!darkMode);
   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      setQuery(searchTerm);
+      setSearchTerm("");
+    }
+  };
 
   const themeClass = "darkLight rounded-full text-xl ";
 
+  useEffect(() => {
+    if (data) {
+      console.log("Yangi data:", data);
+    }
+  }, [data]);
   return (
     <section className="darkLight px-3 md:px-6 py-0.5 md:py-1 flex items-center justify-between">
       <Link to="/" className="flex">
@@ -40,23 +56,32 @@ const Navbar = () => {
           src="/assets/WeatherAppLogo.png"
           alt="Logo"
         />
-        <img className="h-8 md:h-10" src="/assets/WeatherAppW.png" alt="Logo" />
+        <img
+          className="hidden md:flex h-8 md:h-10"
+          src="/assets/WeatherAppW.png"
+          alt="Logo"
+        />
       </Link>
-      <form className="flex items-center relative gap-6">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center relative gap-0 md:gap-6"
+      >
         <h1 className="flex items-center cursor-pointer">
           <FaLocationDot className="mr-1.5" />
           {`${curLocation.city}, ${curLocation.country}`}
         </h1>
         <input
           type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search city..."
-          className=" py-0.5 px-5 rounded-2xl outline-none  bg-hover"
+          className=" py-0.5 px-1 md:px-5 rounded-2xl outline-none  bg-hover"
         />
         <button className="absolute right-4" type="submit">
           <FaSearch />
         </button>
       </form>
-      <menu className="flex items-center gap-6">
+      <menu className="lg:flex items-center gap-6 hidden">
         <button
           onClick={switcher}
           className="flex items-center bg-hover rounded-2xl border-2 border-hover  gap-2 cursor-pointer "
